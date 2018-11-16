f35311ff9feaa22452668f97c25b798372eb6422
angel9999 <31519796+angel9999@users.noreply.github.com>
2018-11-16 11:12:15 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -6,7 +6,7 @@ app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  
 
 @app.route("/")
 def render_main():
-    return render_template('2.html')
+    return render_template('index.html')
 
 if __name__=="__main__":
     app.run(debug=True, port=54321)
